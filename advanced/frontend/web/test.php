6f79d743f0e8c9aac649d6691dc6ca76b7ddf7ee
litao <litao@waixie.com>
2016-12-19 17:31:57 +0800

    增加爬虫文件夹

--- advanced/frontend/web/test.php
+++ advanced/frontend/web/test.php
@@ -1,7 +1,24 @@
 <?php
-$req = $_POST;
-// echo $req;
+class Person{
+	
+}
 
-echo json_encode( $req );
+class Person2 {
+	$name = 'xxx';
 
+	public function foo(){
+		echo $this -> name;
+	}
+}
+
+class Student extends Person implements Person2{
+	$name = 'xxx';
+	
+	public function foo(){
+		echo $this -> name;	
+	}
+}
+$s1 = new Student;
+echo $s1 -> sayName(); // 可以输出xxx
+echo $s1 -> say();  // 报错
 ?>
