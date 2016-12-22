30244905814259b0b6e8d89628c3986a0ab789ed
ReakingAd <ltvieri@163.com>
2016-12-22 22:08:08 +0800

    增加pm2.5数据api对接

--- src/js/list/list.js
+++ src/js/list/list.js
@@ -13,6 +13,7 @@
 		init:function(){
 			this.showKeyword();
 			this.showKeywordPage();
+			this.getPm2_5();
 		},
 		// 创建关键词标签
 		showKeyword:function(){
@@ -32,6 +33,30 @@
 
 				window.location.href = lt_global.baseurl + '/list/tags/' + _keyword;
 			});			
+		},
+		getPm2_5:function(){
+			var params = {
+				token:'5j1znBVAsnSf5xQyNQyq',
+				city:'beijing'
+			}
+			$.ajax({
+				url:'../post/aqi',
+				type:'get',
+				dataType:'json',
+				cache:true,
+				success:function(data){
+					// console.log(data);
+					var pm2_5;
+
+					if( data && !$.isEmptyObject(data) ){
+						pm2_5 = data[data.length-1]['pm2_5'];
+						$('.pm2_5').html(pm2_5);
+					}
+					else{
+						console.warn('获取pm2.5数据出错。');
+					}
+				}
+			});
 		}
 	}
 }).call(this);
